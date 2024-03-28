71e90ebfdb37e1cb4c018f3c88a7fff7b5a6eccb
ROmm123 <romlitae2007@gmail.com>
2024-03-28 18:17:05 +0200

    liddor idea

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
-
 import socket
 import threading
-
+import time
 
 class Server:
     def __init__(self, host, port):
@@ -10,54 +9,50 @@ class Server:
         self.server_socket.listen(5)
         self.clients = []
         self.clients_lock = threading.Lock()
-        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("Server initialized")
+        self.enemies_num = -1
 
     def handle_client(self, client_socket):
         while True:
-
-            data = client_socket.recv(2048)
+            data = client_socket.recv(2048).decode("utf-8")
+            if not data:
+                client_socket.close()
+                break
             # data to another client
-
-            packet = self.added_client(data)
             if len(self.clients) > 1:
-                for receiver_socket, addr in self.clients:
+                updated_packet = self.add_enemies_to_packet(data)
+                for receiver_socket in self.clients:
                     if receiver_socket != client_socket:
-                        receiver_socket.send(packet.encode("utf-8"))
+                        receiver_socket.send(updated_packet.encode("utf-8"))
+                        time.sleep(0.4)
             else:
                 data = "0"
-                client_socket.send(data.encode())
+                client_socket.send(data.encode("utf-8"))
 
     def start(self):
         try:
             while True:
-                print("Waiting for new client...")
                 client_socket, addr = self.server_socket.accept()
-                print(f"New client connected: {addr}")
                 with self.clients_lock:
-                    self.clients.append((client_socket, addr))
+                    self.clients.append(client_socket)
+                self.enemies_num+=1
                 client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
                 client_thread.start()
         except KeyboardInterrupt:
-            print("Server stopped")
+            pass
         finally:
-            print("Closing client sockets...")
             with self.clients_lock:
-                for client_socket, _ in self.clients:
+                for client_socket in self.clients:
                     client_socket.close()
 
             # Close the server socket
             self.server_socket.close()
 
-            print("Server socket closed")
-
-    def added_client(self, data):
-        count = len(self.clients)
-        packet = data.decode("utf-8") + "&" + str(count)
+    def add_enemies_to_packet(self ,data):
+        packet = data+("&"+str(self.enemies_num))
         return packet
+
 
 
 if __name__ == '__main__':
     my_server = Server('localhost', 10023)
-    print("Starting server...")
     my_server.start()
