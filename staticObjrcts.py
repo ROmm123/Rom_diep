40a7c83df8898b7d5a7a3c9dd5d2d6f59d9e3a67
AvisHhhAy <155193022+AvisHhhAy@users.noreply.github.com>
2024-03-30 00:21:14 +0300

    done

--- staticObjrcts.py
+++ staticObjrcts.py
@@ -6,9 +6,9 @@ from HP import HP
 class StaticObject():
     def __init__(self, setting, map_width, map_height):
         # Generate random coordinates of x,y pos in the map range
-        self.width = 50  # Width of the rectangle
-        self.height = 50  # Height of the rectangle
-        self.color = random.choice([setting.red, setting.green, setting.blue])  # random col
+        self.width = 30  # Width of the rectangle
+        self.height = 30  # Height of the rectangle
+        self.color = (3,3,3)
         self.position = (random.randint(0, map_width - 20)  # Random x-coordinate
                          , random.randint(0, map_height - 20))  # Random y-coordinate
         self.HP = HP((self.position[0] + self.width // 2), (self.position[1] + self.height // 2), self.width // 2,
@@ -22,7 +22,7 @@ class StaticObjects():
     def __init__(self, setting, map_width, map_height):
         self.surface = setting.surface
         self.Static_objects = []  # the static object list
-        for _ in range(100):
+        for _ in range(2000):
             obj = StaticObject(setting, map_width, map_height)
             print(obj.position)
             self.Static_objects.append(obj)
@@ -32,7 +32,7 @@ class StaticObjects():
         for static_obj in self.Static_objects:
             obj_x = static_obj.position[0] - viewport_x
             obj_y = static_obj.position[1] - viewport_y
-            if 0 <= obj_x <= setting.screen_width and 0 <= obj_y <= setting.screen_height:
+            if -25 <= obj_x <= setting.screen_width + 20 and -25 <= obj_y <= setting.screen_height + 20:
                 pygame.draw.rect(self.surface, static_obj.color, (obj_x, obj_y, static_obj.width, static_obj.height))
                 pygame.draw.rect(self.surface, static_obj.HP.LifeColor,
                                  (obj_x - (static_obj.width // 2), (obj_y + (static_obj.height + 10)),
